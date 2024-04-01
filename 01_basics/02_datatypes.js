1d0118d0ad940b04cb51a2bc8734c6175a2e762c
Sumit <165494562+sumitwankar@users.noreply.github.com>
2024-04-01 21:43:24 +0000

    learned about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+ "use strict"; // treat all JS code as newer versions
+
+ // alert( 3 + 3) // we are using nodejs, not browser
+
+ console.log(3
+    + 
+    3); // code readability should be high
+
+console.log("Just print It");
+
+let name = "Sumit"
+let age = 30
+let isLoggedIn = true
+let state; // undefined 
+
+// **datatypes**
+
+// number => 2 to power 53
+// BigInt => for big number
+// string => "string"
+// boolean => true/false
+// null => standalone value 
+// undefined => empty
+// Symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
