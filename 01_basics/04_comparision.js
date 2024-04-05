275b8f7a8df48230ff7356f934552ffa460d1fc6
Sumit <165494562+sumitwankar@users.noreply.github.com>
2024-04-05 07:55:44 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1); 
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);  // null = empty
+console.log(null == 0); // null = empty
+console.log(null >= 0); // null converted into 0 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === > check datatype also
+console.log("2" === 2);
