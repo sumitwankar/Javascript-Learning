853e58fa781664e6dd1fff36d2d4911794c9492d
Sumit <165494562+sumitwankar@users.noreply.github.com>
2024-04-19 09:34:44 +0000

    Nested scope and mini hoisting in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,8 +5,51 @@ let b = 200
 if (true){
     var a = 10;
     let b = 20
-    console.log("Inside value = ", b);
+    // console.log("Inside value = ", b);
 }
 
 // console.log(a);
-console.log(b)
+// console.log(b)
+
+// Nested scope 
+function one(){
+    const usrename = "Sumit"
+
+    function two(){
+        const website = "just.com"
+        console.log(usrename);
+    }
+
+    // console.log(website); // inaccessible because it outside the scope
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "Sumit"
+
+    if(username === "Sumit"){
+        const website = "just.com"
+        console.log(username + website);
+    }
+
+    // console.log(website); // inaccessible
+}
+// console.log(username); // inaccessible
+
+
+// ******** Hoisting(Important) ********
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+addtwo(10)
+
+const addtwo = function(num){
+    return num + 2
+}
