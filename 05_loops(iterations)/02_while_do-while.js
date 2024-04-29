7539a53f8ab9bc696de3bebad9d4377a6d0df30e
Sumit <165494562+sumitwankar@users.noreply.github.com>
2024-04-29 17:53:56 +0000

    while, do while lopps

---
+++ 05_loops(iterations)/02_while_do-while.js
@@ -0,0 +1,32 @@
+// ***while
+
+index = 0
+while(index <= 10){
+    // console.log(`Value of index ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ["thor", "Antman", "hulk"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1 
+}
+
+
+// ***do-while
+let score = 1
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+let mark = 11
+
+do {
+    console.log(`Score is ${mark}`);
+    mark++
+} while (mark <= 10);
