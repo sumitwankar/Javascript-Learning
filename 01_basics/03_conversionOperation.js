0f71afd5ebd33640c91f6fd09e594e8e7f5211a0
Sumit <165494562+sumitwankar@users.noreply.github.com>
2024-12-24 17:06:40 +0000

    increment

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -61,15 +61,17 @@ let str2 = " pro"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log(1 + 2); // 3
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 3); // 123
+// console.log(1 + 2 + "3"); // 33
 
 // console.log( (3 + 4) * 5 % 3); // bad code
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); // 1
+// console.log(+""); // 0
 
 let num1, num2, num3
 num1 = num2 = num3 = 2 + 2  // bad code
