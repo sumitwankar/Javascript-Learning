88e7d3585fa388ec4f3a0783b9b163a99b542be8
Sumit <165494562+sumitwankar@users.noreply.github.com>
2024-12-24 17:24:08 +0000

    add some notes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,20 +1,24 @@
-// console.log(2 > 1); 
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
 
 
-console.log(null > 0);  // null = empty
-console.log(null == 0); // null = empty
-console.log(null >= 0); // null converted into 0 
+console.log(null > 0);  // false    null = empty
+console.log(null == 0); // false    null = empty
+console.log(null >= 0); // true     null converted into 0 
+// The reason is that equality check == and comparision > < >= <= work differently.
+// Comparisions convert null to a number, treating it as 0.
+// Thats why (3) null >=0 is true and (1) null > 0 i false 
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
 
 // === > check datatype also
-console.log("2" === 2);
+console.log("2" === 2); // false
