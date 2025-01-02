3d425e492e280b166f3ae5848060bb6ec1f1c9ad
Sumit <165494562+sumitwankar@users.noreply.github.com>
2025-01-02 20:27:31 +0000

    note add

--- 01_basics/06_number_and_maths.js
+++ 01_basics/06_number_and_maths.js
@@ -18,7 +18,7 @@ const numb = 1000000
 
 // ***********maths************
 
-console.log(Math);
+console.log(Math);  // support only browser
 console.log(Math.abs(-4));
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.2));
