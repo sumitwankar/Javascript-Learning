95958a28f8b22b99743446ff615332789116170d
Sumit <165494562+sumitwankar@users.noreply.github.com>
2025-01-02 21:02:42 +0000

    add note

--- 02_basics/02_array_more.js
+++ 02_basics/02_array_more.js
@@ -1,27 +1,33 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros) // its takes array as an element
 // Output = [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
 
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][2]);
+// console.log(marvel_heros[3][2]);  // accessing value of inside array
 
-// const allheros = marvel_heros.concat(dc_heros)
+
+// const allheros = marvel_heros.concat(dc_heros)  // it's merg two array
 // console.log(allheros);
 
-// const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// const all_new_heros = [...marvel_heros, ...dc_heros] // new way to concat two array 
 // console.log(all_new_heros); // similar to concat
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+// const real_another_array = another_array.flat(2)  // set depth in flat method,and this is proper way to use flat method
+const real_another_array = another_array.flat(Infinity) // all array convert into one array
 console.log(real_another_array);
 
 
-console.log(Array.isArray("Sumii"))
-console.log(Array.from("Sumit"))
-console.log(Array.from({name: "Sumit"})) // interesting
+console.log(Array.isArray(marvel_heros)); // check array or not
+console.log(Array.isArray("Sumii"))  // check array or not
+
+console.log(Array.from("Sumit")) // convert given value in array
+console.log(Array.from({name: "Sumit"})) // interview interesting, output will be empty bracket, beacause we need to tell specifiac value to convert into array
+
 
 let score1 = 100
 let score2 = 200
