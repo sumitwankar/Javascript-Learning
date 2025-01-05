903c4952390a6d4580107a739ba05277055d9c28
Sumit <165494562+sumitwankar@users.noreply.github.com>
2025-01-05 19:35:51 +0000

    add note about object

--- 02_basics/04_object_more.js
+++ 02_basics/04_object_more.js
@@ -33,12 +33,18 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)  // merging multiple object
 
+// const objName = ({}, obj1, obj2, obj3, .....) //always use {} as target object as source.
+
+
+// spread operator  
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
 
+
+// Multiple Object in array
 const users = [
     {
         id: 1,
@@ -58,9 +64,18 @@ const users = [
 users[1].email
 console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-// // console.log(tinderUser.hasOwnProperty('isLogged'));
+/*** Object Method and Properties ***/
+
+console.log(Object.keys(tinderUser));   //  [ 'id', 'name', 'isLoggedIn' ]
+// Object.keys() method returns an array with the keys of an object.
+
+console.log(Object.values(tinderUser));  // [ '123abc', 'Sammy', false ]
+// Object.values() method returns an array of the property values of an object.
+
+console.log(Object.entries(tinderUser));  //  [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+// Object.entries() method returns an array of the key/value pairs of an object.
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // check object own property, it return in true/false
+// console.log(tinderUser.hasOwnProperty('isLogged'));
